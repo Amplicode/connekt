8d08ff94a31718dac71e3d4fcfdaea8c86799910
Aleksandr Magomedov <alx.serg.mag@gmail.com>
2025-08-28 18:54:17 +0400

    Update connekt.bat

--- install/connekt.bat
+++ install/connekt.bat
@@ -13,29 +13,29 @@ rem === PARSE ARGS ===
 if "%~1"=="" goto :after_args
 
 if /i "%~1"=="-env" (
-set "ENV_FILE=%~2"
-shift
-shift
-goto :parse_args
+  set "ENV_FILE=%~2"
+  shift
+  shift
+  goto :parse_args
 )
 
 if /i "%~1"=="-envname" (
-set "ENV_NAME=%~2"
-shift
-shift
-goto :parse_args
+  set "ENV_NAME=%~2"
+  shift
+  shift
+  goto :parse_args
 )
 
 if /i "%~1"=="-envparams" (
-shift
-:envparam_loop
-if "%~1"=="" goto :parse_args
-if "%~1"=="-env" goto :parse_args
-if "%~1"=="-envname" goto :parse_args
-if "%~1"=="-envparams" goto :parse_args
-set "ENV_PARAM_ARGS=!ENV_PARAM_ARGS! --env-param %~1"
-shift
-goto :envparam_loop
+  shift
+  :envparam_loop
+  if "%~1"=="" goto :parse_args
+  if /i "%~1"=="-env" goto :parse_args
+  if /i "%~1"=="-envname" goto :parse_args
+  if /i "%~1"=="-envparams" goto :parse_args
+  set "ENV_PARAM_ARGS=!ENV_PARAM_ARGS! --env-param %~1"
+  shift
+  goto :envparam_loop
 )
 
 rem default: treat as script
@@ -48,41 +48,45 @@ goto :parse_args
 rem === Check if Docker image exists locally ===
 docker image inspect "%IMAGE%" >nul 2>&1
 if errorlevel 1 (
-echo Docker image %IMAGE% not found locally. Pulling...
-docker pull "%IMAGE%"
+  echo Docker image %IMAGE% not found locally. Pulling...
+  docker pull "%IMAGE%"
 )
 
 rem === Prepare ENV_FILE mount ===
 set "ENV_FILE_MOUNT="
 set "ENV_FILE_ARG="
 if not "%ENV_FILE%"=="" (
-for %%F in ("%ENV_FILE%") do set "ENV_FILE_PATH=%%~fF"
-set "ENV_FILE_MOUNT=-v !ENV_FILE_PATH!:/connekt/scripts/connekt.env.json"
-set "ENV_FILE_ARG=--env-file=scripts/connekt.env.json"
+  for %%F in ("%ENV_FILE%") do set "ENV_FILE_PATH=%%~fF"
+  set "ENV_FILE_MOUNT=-v !ENV_FILE_PATH!:/connekt/scripts/connekt.env.json"
+  set "ENV_FILE_ARG=--env-file=scripts/connekt.env.json"
 )
 
 rem === Prepare ENV_NAME arg ===
 set "ENV_NAME_ARG="
 if not "%ENV_NAME%"=="" (
-set "ENV_NAME_ARG=--env-name=%ENV_NAME%"
+  set "ENV_NAME_ARG=--env-name=%ENV_NAME%"
 )
 
 rem === Run each script ===
 for %%S in (%SCRIPT_ARGS%) do (
-for %%F in ("%%~fS") do (
-set "DIR=%%~dpF"
-set "FILE=%%~nxF"
-echo Running !FILE! ...
-docker run --rm ^
---add-host=host.docker.internal:host-gateway ^
--v "!DIR!:/connekt/scripts" ^
-%ENV_FILE_MOUNT% ^
-%IMAGE% ^
-%ENV_NAME_ARG% ^
-%ENV_FILE_ARG% ^
---script=scripts/!FILE! ^
-%ENV_PARAM_ARGS%
-)
+  for %%F in ("%%~fS") do (
+    set "DIR=%%~dpF"
+    set "FILE=%%~nxF"
+    echo Running !FILE! ...
+
+    set "CMD=docker run --rm"
+    set "CMD=!CMD! --add-host=host.docker.internal:host-gateway"
+    set "CMD=!CMD! -v "!DIR!:/connekt/scripts""
+    if defined ENV_FILE_MOUNT set "CMD=!CMD! %ENV_FILE_MOUNT%"
+    set "CMD=!CMD! %IMAGE%"
+    if defined ENV_NAME_ARG set "CMD=!CMD! %ENV_NAME_ARG%"
+    if defined ENV_FILE_ARG set "CMD=!CMD! %ENV_FILE_ARG%"
+    set "CMD=!CMD! --script=scripts/!FILE!"
+    if defined ENV_PARAM_ARGS set "CMD=!CMD! %ENV_PARAM_ARGS%"
+
+    rem Execute the command
+    !CMD!
+  )
 )
 
 exit /b 0
