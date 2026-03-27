bb6b6b8a70c926efec644963abdebb2acde61b5c
subbotin <a.subbotin@haulmont.com>
2026-03-27 13:56:15 +0400

    improve json protocol

--- connekt-script-definition/src/main/java/io/amplicode/connekt/JsonOutputConnektInterceptor.kt
+++ connekt-script-definition/src/main/java/io/amplicode/connekt/JsonOutputConnektInterceptor.kt
@@ -13,27 +13,33 @@ class JsonOutputConnektInterceptor(
 ) : ConnektInterceptorBase(printer, responseStorageDir, requestStorageDir) {
 
     private val objectMapper = jacksonObjectMapper()
+    private var requestCount = 0
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
         val id = UUID.randomUUID().toString()
+        val idx = ++requestCount
 
-        emitRequestEvent(request, id)
+        emitRequestEvent(request, id, idx)
 
         val response = try {
             chain.proceed(request)
         } catch (e: Exception) {
             printer.println(objectMapper.writeValueAsString(
-                mapOf("event" to "error", "id" to id, "message" to "Connection refused")
+                mapOf(
+                    "event" to "error",
+                    "id" to id,
+                    "index" to idx,
+                    "message" to "Connection refused")
             ))
             throw e
         }
 
-        emitResponseEvent(response, id)
+        emitResponseEvent(response, id, idx)
         return response
     }
 
-    private fun emitRequestEvent(request: Request, id: String) {
+    private fun emitRequestEvent(request: Request, id: String, idx: Int) {
         val headers = request.headers.map { listOf(it.first, it.second) }
 
         val bodyInfo = prepareRequestBody(request)
@@ -41,6 +47,7 @@ class JsonOutputConnektInterceptor(
         val event = mutableMapOf(
             "event" to "request",
             "id" to id,
+            "index" to idx,
             "method" to request.method,
             "url" to request.url.toString(),
             "headers" to headers,
@@ -51,7 +58,7 @@ class JsonOutputConnektInterceptor(
         printer.println(objectMapper.writeValueAsString(event))
     }
 
-    private fun emitResponseEvent(response: Response, id: String) {
+    private fun emitResponseEvent(response: Response, id: String, idx: Int) {
         val headers = response.headers.map { listOf(it.first, it.second) }
 
         val bodyInfo = prepareResponseBody(response)
@@ -59,6 +66,7 @@ class JsonOutputConnektInterceptor(
         val event = mutableMapOf(
             "event" to "response",
             "id" to id,
+            "index" to idx,
             "status" to response.code,
             "statusText" to response.message,
             "protocol" to response.formatProtocol(),
